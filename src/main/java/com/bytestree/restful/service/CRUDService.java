98241e1a0eb7a50c1ccd7de46408fb5bbd75fba3
mpkuma <mannavapavankumar@gmail.com>
2018-09-15 20:55:29 +0530

    first commit

---
+++ src/main/java/com/bytestree/restful/service/CRUDService.java
@@ -0,0 +1,28 @@
+package com.bytestree.restful.service;
+
+
+/**
+ * @author BytesTree
+ */
+
+import java.io.Serializable;
+
+import java.util.List;
+
+
+public interface CRUDService<E>
+ {
+
+	
+E save(E entity);
+
+
+	E getById(Serializable id);
+
+
+	List<E> getAll();
+
+	
+void delete(Serializable id);
+
+}
